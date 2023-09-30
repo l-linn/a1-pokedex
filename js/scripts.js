a0ed6214d7343303e2c919574d9a4f3155890f9f
lin <shilinxi@proton.me>
2023-09-30 20:13:16 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 
 // name, height, type
-
 let pokemonList1 = [
     {
         name: 'Lunatone',
@@ -32,7 +31,6 @@ let pokemonList1 = [
     }
 ];
 
-
 let pokemonList2 = [
     {
         name: 'Pichu',
@@ -62,12 +60,12 @@ let pokemonList2 = [
         types: ['ice','water'],
         evolutions: 'Spheal evolves into Sealeo at level 32',
     }
-];
-
+]; 
 
 console.log(pokemonList1);
 
-/*for (let i = 0; i < pokemonList.length; i++) {
+/* for loop
+for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 0.5 && pokemonList1[i].height < 1) {
         document.write(pokemonList1[i].name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
     } else if (pokemonList[i].height === 1) {
@@ -77,10 +75,10 @@ console.log(pokemonList1);
     } else {
         document.write(pokemonList1[i].name + ' is taller than 1 meter - wow that\'s big!<br>');
     };
-}*/
+}
 
-
-/*let i = 0;
+for loop - while
+let i = 0;
 while (i < pokemonList.length){
     if (pokemonList[i].height >= 0.5 && pokemonList1[i].height < 1) {
         document.write(pokemonList1[i].name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
@@ -93,12 +91,10 @@ while (i < pokemonList.length){
     };
 
     i++;
-}*/
+}
 
-
-// function 
-/*function printListDetails(){
-    let i = 0;
+for loop - do while
+let i = 0;
 do {
     if (pokemonList[i].height >= 0.5 && pokemonList1[i].height < 1) {
         document.write(pokemonList1[i].name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
@@ -115,7 +111,27 @@ do {
 while (i < pokemonList1.length); // 5
 };
 
-//print twice
+*/
+
+/*function printListDetails(){
+    let i = 0;
+do {
+    if (pokemonList1[i].height >= 0.5 && pokemonList1[i].height < 1) {
+        document.write(pokemonList1[i].name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
+    } else if (pokemonList[i].height === 1) {
+        document.write(pokemonList1[i].name + ' is 1 meter tall.<br>');
+    } else if (pokemonList1[i].height < 0.5) {
+        document.write(pokemonList1[i].name + ' is shorter than 0.5 meter.<br>');
+    } else {
+        document.write(pokemonList1[i].name + ' is taller than 1 meter - wow that\'s big!<br>');
+    };
+
+    i++
+}
+while (i < pokemonList1.length); // 5
+};
+
+//print pokemonList1 twice
 printListDetails();
 printListDetails();
 */
@@ -144,4 +160,38 @@ pokemonList1.forEach(function(pokemonName) {
 });
 //arrow function
 pokemonList2.forEach(pokemonHeight => document.write(pokemonHeight.height));
+
+//Immediately Invoked Function Expression (or IIFE) IIFE is a function
+let pokemonRepo = (function(){
+    let pokemonList = [
+        {  
+            name: 'Lunatone',
+            number: '#337',
+            height: 1,
+            types: ['psychic', 'rock'],
+            evolutions: 'none',
+        },
+        {
+            name: 'Kirilia',
+            number: '#281',
+            height: 0.8,
+            types: ['psychic', 'fairy'],
+            evolutions: 'Kirlia evolves into Gardevoir at level 30',
+        },
+        {
+            name: 'Pikachu',
+            number: '#25',
+            height: 0.4,
+            types: ['electric'],
+            evolutions: 'Pichu evolves into Pikachu when leveled up with high friendship',
+        },
+        {
+            name: 'Rapidash',
+            number: '#78',
+            height: 1.7,
+            types: ['fire'],
+            evolutions: 'Ponyta evolves into Rapidash at level 40',
+        }
+    ]
+})()
 
