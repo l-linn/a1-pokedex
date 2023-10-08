c7ddf416be327086c491cb628715621e75d7628e
lin <shilinxi@proton.me>
2023-10-08 20:52:51 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,16 @@
 //Immediately Invoked Function Expression (or IIFE) IIFE is a function
 let pokemonRepo = (function(){
-    let pokemonList = [
-        {  
-            name: 'Lunatone',
-            number: '#337',
-            height: 1,
-            types: ['psychic', 'rock'],
-            evolutions: 'none',
-        },
-        {
-            name: 'Kirilia',
-            number: '#281',
-            height: 0.8,
-            types: ['psychic', 'fairy'],
-            evolutions: 'Kirlia evolves into Gardevoir at level 30',
-        },
-        {
-            name: 'Pikachu',
-            number: '#25',
-            height: 0.4,
-            types: ['electric'],
-            evolutions: 'Pichu evolves into Pikachu when leveled up with high friendship',
-        },
-        {
-            name: 'Rapidash',
-            number: '#78',
-            height: 1.7,
-            types: ['fire'],
-            evolutions: 'Ponyta evolves into Rapidash at level 40',
-        },
-        {
-            name: 'Togekiss',
-            number: '#468',
-            height: 1.5,
-            types: ['fairy',' flying'],
-            evolutions: 'Togetic evolves into Togekiss using a stone',
-        }
-    ]
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-    function getAll() {
+    function getAll(){
         return pokemonList;
     }
 
-    function add(newPokemon) {
+    function add(newPokemon){
         if (
             typeof newPokemon === 'object' && 
-            'name' in newPokemon &&
-            'height' in newPokemon &&
-            'types' in newPokemon
+            'name' in newPokemon
             ) {
             pokemonList.push(newPokemon);
         } else {
@@ -55,21 +18,21 @@ let pokemonRepo = (function(){
         };
     }
 
-    function showDetails (item) {
-        console.log(item);
+    function showDetails(pokemon){
+        loadDetails(pokemon).then(function(){
+          console.log(pokemon);
+        });
     }
 
-    function addListItem (item){
+    function addListItem(item){
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
 
         //button.addEventListener('click', () => alert('you clicked the button'));
         button.addEventListener('click', function() {
-            console.log(item);
-        })
-
-        button.addEventListener('click', showDetails(item));
+            showDetails(item);
+        });
 
         button.innerText = item.name;
         button.classList.add('button-class');
@@ -77,56 +40,51 @@ let pokemonRepo = (function(){
         pokemonList.appendChild(listItem); //append the list item to the unordered list as its child.
     }
 
+    function loadList(){
+        return fetch(apiUrl).then(function(response){
+            return response.json();
+        }).then(function(json){
+            json.results.forEach(function(item){
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url,
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });
+        }).catch(function(e){
+            console.error(e)
+        })
+    }
+
+    function loadDetails(item){
+        let url = item.detailsUrl;
+        return fetch(url).then(function(response){
+            return response.json();
+        }).then(function(details){
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function(e){
+            console.error(e);
+        });
+    }
 
     return {//this is an object
         getAll: getAll,
         add: add,
         addListItem: addListItem,
-    }
-})()
+        loadList: loadList,
+        loadDetails: loadDetails
+    };
+})();
 
 console.log(pokemonRepo.getAll());
 
-//pokemonRepo.getAll().forEach(pokemon => document.write(pokemon.name + ', ' + pokemon.height + 'm, ' + 'is ' + pokemon.types + ' type<br>'));
-/*
-function heightLoop(item){
-    if (item.height >= 0.5 && item.height < 1) {
-        document.write(item.name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
-    } else if (item.height === 1) {
-        document.write(item.name + ' is 1 meter tall.<br>');
-    } else if (item.height < 0.5) {
-        document.write(item.name + ' is shorter than 0.5 meter.<br>');
-    } else {
-        document.write(item.name + ' is taller than 1 meter - wow that\'s big!<br>');
-    };
-};
-*/
-//pokemonRepo.getAll().forEach(heightLoop);
-
-let newPokemon = {
-    name: 'Sealeo',
-    number: '#364',
-    height: 1.1,
-    types: ['ice',' water'],
-    evolutions: 'Spheal evolves into Sealeo at level 32',
-}
-
-pokemonRepo.add(newPokemon); //pokemonRepo.getAll().push(newPokemon) gets the same result
-pokemonRepo.add(177);
-
-pokemonRepo.getAll().forEach(function (item) {
-    pokemonRepo.addListItem(item)
-/*
-    if (item.height >= 0.5 && item.height < 1) {
-        document.write(item.name + ' is shorter than 1 meter but higher than 0.5 meter.<br>');
-    } else if (item.height === 1) {
-        document.write(item.name + ' is 1 meter tall.<br>');
-    } else if (item.height < 0.5) {
-        document.write(item.name + ' is shorter than 0.5 meter.<br>');
-    } else {
-        document.write(item.name + ' is taller than 1 meter - wow that\'s big!<br>');
-    };
-*/
+pokemonRepo.loadList().then(function(){
+    pokemonRepo.getAll().forEach(function(pokemon){
+        pokemonRepo.addListItem(pokemon);
+    });
 });
 
 //changing color of h1 on mouseover
@@ -135,12 +93,4 @@ button.addEventListener('mouseover', function(colChange){
     let target = colChange.target;
     target.classList.toggle('color__light');
     target.classList.toggle('color__dark');
-});
-
-fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
-  return response.json(); // This returns a promise!
-}).then(function (pokemonList) {
-  console.log(pokemonList); // The actual JSON response
-}).catch(function () {
-  // Error
 });
