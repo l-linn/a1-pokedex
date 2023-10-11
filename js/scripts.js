c70c0ab6524fa042f48254cdef9d5798dd7a4438
lin <shilinxi@proton.me>
2023-10-11 23:37:42 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,21 +21,36 @@ let pokemonRepo = (function(){
     function showDetails(pokemon){
         loadDetails(pokemon).then(function(){
           console.log(pokemon);
+          //let pokemonHeight = pokemon.height;
+          //document.write(pokemonHeight); this opens a new window
         });
     }
 
     function addListItem(item){
+
+        loadDetails(item).then(function(){
+            let img = document.createElement('img');
+            let imgSrc = item.imageUrl; //item.detailsUrl is the link to the image;
+            img.src = imgSrc;
+            img.classList.add('img-class');
+            listItem.appendChild(img);
+        })
+
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
+        //if I want to add multipul items inside the button:
+        //let name = document.createElement('p');
+        //name.innerText = item.name;//display pokemon's name
+        //name.classList.add('name-class');
+        //button.appendChild(name);
 
-        //button.addEventListener('click', () => alert('you clicked the button'));
-        button.addEventListener('click', () => showDetails(item));
-        
         button.innerText = item.name;
         button.classList.add('button-class');
-        listItem.appendChild(button); //append the button to the list item as its child.
         pokemonList.appendChild(listItem); //append the list item to the unordered list as its child.
+        listItem.appendChild(button); //append the button to the list item as its child.
+        //button.addEventListener('click', () => alert('you clicked the button'));
+        button.addEventListener('click', () => showDetails(item));
     }
 
     function loadList(){
@@ -48,24 +63,23 @@ let pokemonRepo = (function(){
                     detailsUrl: item.url,
                 };
                 add(pokemon);
-                console.log(pokemon);
+                //console.log(pokemon); logs all added pokemons
             });
         }).catch(function(e){
             console.error(e)
         })
     }
 
-    function loadDetails(item){
-        let url = item.detailsUrl;
-        return fetch(url).then(function(response){
-            return response.json();
+    function loadDetails(addedPokemon){
+        let url = addedPokemon.detailsUrl; //.detailsUrl is created in loadList dunction as a key, to get the url in each pokemon object
+        return fetch(url).then(function(response){// to fetch the url inside each pokemon object
+            //console.log(response);
+            return response.json();//this returns a promise
         }).then(function(details){
-            item.imageUrl = details.sprites.front_default;
-            item.height = details.height;
-            item.types = details.types;
-        }).catch(function(e){
-            console.error(e);
-        });
+            addedPokemon.imageUrl = details.sprites.front_default;
+            addedPokemon.height = details.height;
+            addedPokemon.types = details.types;
+        }).catch(() => console.error(e));
     }
 
     return {//this is an object
@@ -77,7 +91,7 @@ let pokemonRepo = (function(){
     };
 })();
 
-console.log(pokemonRepo.getAll());
+//console.log(pokemonRepo.getAll());
 
 pokemonRepo.loadList().then(function(){
     pokemonRepo.getAll().forEach(function(pokemon){
