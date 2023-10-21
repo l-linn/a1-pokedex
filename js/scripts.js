87bb54d9a84d7ca6de8fe127b898d8aee2a7f9bb
lin <shilinxi@proton.me>
2023-10-21 11:31:32 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -116,7 +116,6 @@ pokemonRepo.loadList().then(function(){
 (function(){
     let bgColorChange = document.querySelector('.navbar-brand, nav img');
     let bg = document.querySelector('ul');
-
     bgColorChange.addEventListener('mouseover', () => {
         bg.classList.toggle('first-color');
         bg.classList.toggle('second-color');
